48bb9fe7dafbef47751a1cd017f6edffccd41fc8
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-06-18 22:49:50 +0530

    Explored methods involving sockets

--- chat-app/client.js
+++ chat-app/client.js
@@ -1,10 +1,19 @@
  const net = require("net");
-
+const readline = require("readline/promises")
  
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+})
 
- const client = net.createConnection({host:"127.0.0.1",port:6969},()=>{
+ const client = net.createConnection({host:"127.0.0.1",port:6969},async ()=>{
     console.log("Connection created")
-    // client.end()
+
+    const message = await rl.question("Enter a message  > ");
+    client.write(message);
+    const message2 = await rl.question("Enter a message  > ");
+    client.write(message2);
+    client.end()
  })
 
  client.on("close",()=>{
--- chat-app/server.js
+++ chat-app/server.js
@@ -9,9 +9,17 @@ let i = 0
 
 server.on("connection",(socket)=>{
     console.log("A new connection was made.." + i);
-    i++;
+    socket.on("data",(data)=>{
+        console.log(data.toString("utf-8"))
+    })
+    socket.on("close",()=>{
+        console.log("The client closed connection!")
+    })
+    
     // socket.end()
 })
+
+// server.maxConnections=2;
 
 server.listen(PORT,HOSTNAME,()=>{
     console.log(`Server is up and running at` , server.address())
